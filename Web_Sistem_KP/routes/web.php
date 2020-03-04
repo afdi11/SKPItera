b1d970da5fdadeacee3e1fcd693319f0c20d9514
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2020-03-05 06:58:54 +0700

    tampilan login dan register

--- Web_Sistem_KP/routes/web.php
+++ Web_Sistem_KP/routes/web.php
@@ -14,5 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+/*
+Route::get('login', function () {
+    return view('login');
+});
 
-Route::get('/mahasiswa/instansi', 'MahasiswaInstansi@index');
+Route::get('register', function () {
+    return view('register');
+});
+*/
+Route::get('mahasiswa/instansi', 'MahasiswaInstansi@index');
