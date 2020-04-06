dc0334c6b6578c9c844048030267309aac154f35
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2020-04-06 13:24:44 +0700

    Update index.blade.php

--- Web_Sistem_KP/resources/views/koor/cetak/index.blade.php
+++ Web_Sistem_KP/resources/views/koor/cetak/index.blade.php
@@ -1,30 +1,7 @@
-@extends('layouts.koor.koor_cetak_index')
+@extends('layouts.koor.koor_index')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header container">
-                    <div class="row align-items-center">
-                        <div class="col">
-                            Cetak Nilai
-                        </div>
-                    </div>
-                </div>
 
-                <div class="card-body">
-                <div class="row">
-                    </div class="col-3 col-s-12 menu">
-                        <ul>
-                            <li><a href="{{route('koor.index')}}">Home</a></li>
-                            <li><a href="{{route('koor.user.index')}}">Mahasiswa</a></li>
-                            <li><a href="{{route('koor.laporan.index')}}">Laporan</a></li>
-                            <li><a href="{{route('koor.cetak.index')}}">Cetak Nilai</a></li>
-                            <li><a href="{{route('koor.validasi.index')}}">Validasi</a></li>
-                            <li><a href="{{route('koor.user.index')}}">Dosen Pembimbing</a></li>
-                        </ul>
-                    </div>
                     </div class="col-3 col-s-12 menu">
                         Tampilan Cetak
                     </div>
