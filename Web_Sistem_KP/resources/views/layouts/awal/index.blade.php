cb391dc15fb8de9009054ede77033250f8996d26
Dhiko <dhikojjp@gmail.com>
2020-04-08 09:25:52 +0700

    change href home_page awal

--- Web_Sistem_KP/resources/views/layouts/awal/index.blade.php
+++ Web_Sistem_KP/resources/views/layouts/awal/index.blade.php
@@ -4,16 +4,18 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="{{asset('/css/home_page.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('/css/awal/home_page.css')}}" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <body>
     @yield('content')
     <div class="footer">
         <p><i class="fa fa-copyright" aria-hidden="true"></i> Copyright</p>
     </div>
 </body>
+
 </html>
