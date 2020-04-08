c8515b7c941b28b7015e37f8902f4cd6da0c1f28
Dhiko <dhikojjp@gmail.com>
2020-04-08 10:19:58 +0700

    perbaikan tampilan awal home

--- Web_Sistem_KP/resources/views/index.blade.php
+++ Web_Sistem_KP/resources/views/index.blade.php
@@ -2,26 +2,41 @@
 
 @section('content')
 <div class="header">
-    <h1>Institut Teknologi Sumatera</h1>
+    <div class="col-1 col-s-3 logo">
+        <img src="{{ asset('/requirement/logo_itera.png') }}">
+    </div>
+    <div class="sub">
+        <small>INSTITUT TEKNOLOGI</small>
+        <br>
+        <strong>SUMATERA</strong>
+    </div>
 </div>
 
 <div class="row">
     @guest
-    <form class="col-6 col-s-12 form" action="login">
-        <h4>Website Resmi Kerja Praktik Teknik Informatika
-            Institut Teknologi Sumatera</h4>
-        <button class="btn btn-primary" type="submit">Login</a></button>
-    </form>
-    @else
-    <form class="col-6 col-s-12 form" action="logout" method="POST">
-        <h4>Website Resmi Kerja Praktik Teknik Informatika
-            Institut Teknologi Sumatera</h4>
-        <div class="row">
-            @csrf
-            <a href="login" class="btn btn-primary">Home</a>
-            <button class="btn btn-primary" type="submit">Logout</button>
-        </div>
-    </form>
+    <div class="col-12 col-s-12 abc">
+        <form class="col-6 col-s-12 form" action="login">
+            <strong>WEBSITE RESMI KERJA PRAKTIK TEKNIK INFORMATIKA</strong>
+            <br>
+            <strong>INSTITUT TEKNOLOGI SUMATERA</strong>
+            <br>
+            <br>
+            <button class="btn btn-success" type="submit">Masuk</a></button>
+            <a href="{{ asset('/requirement/file_persetujuan.docx') }}">Unduh Surat Persetujuan</a>
+        </form>
+
+        @else
+        <form class="col-6 col-s-12 form" action="logout" method="POST">
+            <h4>Website Resmi Kerja Praktik Teknik Informatika
+                Institut Teknologi Sumatera</h4>
+            <div class="row">
+                @csrf
+                <a href="login" class="btn btn-primary">Home</a>
+                <button class="btn btn-primary" type="submit">Logout</button>
+                <a href="{{ asset('/requirement/file_persetujuan.docx') }}">Unduh Surat Persetujuan</a>
+            </div>
+        </form>
+    </div>
     @endguest
 
     <div class="col-6 col-s-12 content">
@@ -31,32 +46,34 @@
                 <img src="{{ asset('/requirement/alur_kp.png') }}" alt="Foto Alur KP">
                 <p>Info Alur Kerja Praktik</p>
             </div>
-
-            <a href="{{ asset('/requirement/file_persetujuan.docx') }}">Unduh Surat Persetujuan</a>
         </div>
     </div>
 </div>
-<div class="info_kp">
-    <h4>Info Tempat Kerja Praktik</h4>
-    <table class=" table table-hover">
-        <thead>
-            <tr>
-                <th>No.</th>
-                <th>Instansi</th>
-                <th>Alamat</th>
-                <th>Email</th>
+
+<div style="color:black" class="info_kp">
+    <h4 align="center">Info Tempat Kerja Praktik</h4>
+    <br>
+    <div class="table-responsive">
+        <table class=" table table-hover">
+            <tr class="active">
+                <th width="5%">No</th>
+                <th width="25%">Nama Instansi</th>
+                <th width="35%">Alamat</th>
+                <th width="15%">No. Telepon</th>
+                <th width="20%">Email</th>
             </tr>
-        </thead>
-        <tbody>
-            @foreach($instansi as $p)
-            <tr>
-                <td>{{$p->id}}</td>
-                <td>{{$p->nama}}</td>
-                <td>{{$p->alamat}}</td>
-                <td>{{$p->email}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+            <tbody>
+                @foreach($instansi as $p)
+                <tr>
+                    <td>{{$p->id}}</td>
+                    <td>{{$p->nama}}</td>
+                    <td>{{$p->alamat}}</td>
+                    <td>{{$p->Telp}}</td>
+                    <td>{{$p->email}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 @endsection
