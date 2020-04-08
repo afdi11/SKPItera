199e9677d7ee4ce8f37cac57e4319674ed77382f
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2020-04-08 18:50:13 +0700

    UPDATE FUNGSIONAL MAHASISWA : Unduh form

--- Web_Sistem_KP/resources/views/mahasiswa/mhs_unduh.blade.php
+++ Web_Sistem_KP/resources/views/mahasiswa/mhs_unduh.blade.php
@@ -6,27 +6,27 @@
             <div class="col-12 col-s-12 content_satu">
                 <div class="col-2 col-s-12 nilai_akhir">
                     <p>Penilaian Instansi</p>
-                    <a href="#">Unduh</a>
+                    <a href="{{ asset('/requirement/KP3-Form-Penilaian-Instansi-KP-REV.docx') }}">Unduh</a>
                 </div>
 
                 <div class="col-2 col-s-12 semester">
                     <p>Seminar</p>
-                    <a href="#">Unduh</a>
+                    <a href="{{ asset('/requirement/KP4-Form-Pendaftaran-Seminar-KP-IF-REV.docx') }}">Unduh</a>
                 </div>
 
                 <div class="col-2 col-s-12 catatan">
                     <p>Presensi</p>
-                    <a href="#">Unduh</a>
+                    <a href="{{ asset('/requirement/KP2-Form-Presensi-dan-Log-Sheet-KP-IF-REV-40.docx') }}">Unduh</a>
                 </div>
 
                 <div class="col-2 col-s-12 semester">
-                    <p>Kelengkapan</p>
-                    <a href="#">Unduh</a>
+                    <p>Ceklis Kelengkapan</p>
+                    <a href="{{ asset('/requirement/C1-Ceklis-Kelengkapan-Seminar-Kerja-Praktik.docx') }}">Unduh</a>
                 </div>
 
                 <div class="col-2 col-s-12 catatan">
                     <p>Log Sheet</p>
-                    <a href="#">Unduh</a>
+                    <a href="{{ asset('/requirement/KP2-Form-Presensi-dan-Log-Sheet-KP-IF-REV-40.docx') }}">Unduh</a>
                 </div>
             </div>
         </div>
