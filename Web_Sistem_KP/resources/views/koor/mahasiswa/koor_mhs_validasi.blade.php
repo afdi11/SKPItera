0a88f78dfc7d09ef3be20ed07ba10aeaeb8b32ff
Dhiko <dhikojjp@gmail.com>
2020-04-11 12:02:17 +0700

    perbaikan koor

---
+++ Web_Sistem_KP/resources/views/koor/mahasiswa/koor_mhs_validasi.blade.php
@@ -0,0 +1,8 @@
+<div class="table-responsive">
+    <table class="table table-bordered">
+        <tr>
+            <td width="30%"><label>Nama</label></td>
+            <td width="70%">: {{$model->name}}</td>
+        </tr>
+    </table>
+</div>
--- web_sistem_kp/resources/views/koor/mahasiswa/koor_mhs.blade.php
+++ web_sistem_kp/resources/views/koor/mahasiswa/koor_mhs.blade.php
@@ -5,16 +5,30 @@
     <table class=" col-12 col-s-12 table table-hover">
         <tr>
             <th width="30%">Mahasiswa</th>
-            <th width="25%">Nilai Seminar</th>
-            <th width="25%">Nilai Instansi</th>
+            <th width="25%">Laporan Seminar</th>
+            <th width="25%">Laporan Instansi</th>
             <th width="10%">Validasi</th>
-            <th width="10%">Lainnya</th>
+            <th width="10%">Lihat</th>
         </tr>
         @foreach ($result as $row)
         <tr>
             <td>{{$row->name}}</td>
-            <td>{{$row->name}}</td>
-            <td>{{$row->name}}</td>
+            <td>
+                <div class="form-check">
+                    <label class="form-check-label" for="check1">
+                        <input type="checkbox" class="form-check-input" id="check1" name="option1" value="something">
+                    </label>
+                    <a href="#">Lihat Laporan</a>
+                </div>
+            </td>
+            <td>
+                <div class="form-check">
+                    <label class="form-check-label" for="check2">
+                        <input type="checkbox" class="form-check-input" id="check2" name="option2" value="something">
+                    </label>
+                    <a href="#">Lihat Laporan</a>
+                </div>
+            </td>
             <td>
                 <a href="{{route('koor.mahasiswa.show',$row->user_id)}}" class="view_data_lihat" id="{{$row->id}}" title="Data Mahasiswa">
                     <button class="btn btn-info btn-xs">Validasi</button>
@@ -22,7 +36,7 @@
             </td>
             <td>
                 <a href="{{route('koor.mahasiswa.show',$row->user_id)}}" class="view_data_lihat" id="{{$row->id}}" title="Data Mahasiswa">
-                    <button class="btn btn-info btn-xs">lihat</button>
+                    <button class="btn btn-info btn-xs">Lihat</button>
                 </a>
             </td>
         </tr>
