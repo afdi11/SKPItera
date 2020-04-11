d339a20ac8047b58ee84a4b79193faddce777393
Dhiko <dhikojjp@gmail.com>
2020-04-11 09:14:57 +0700

    perbaikan home page dosen

--- web_sistem_kp/resources/views/dosen/home_page.php
+++ web_sistem_kp/resources/views/dosen/home_page.php
@@ -62,34 +62,11 @@
 
                 <div class="col-3 col-s-12 catatan">
                     <p>Jumlah Mahasiswa Kerja Praktik</p>
-                    <h2>500</h2>
+                    <h2>600</h2>
                 </div>
             </div>
         </div>
 
-    </div>
-
-    <div class="col-12 col-s-12 content_tiga">
-
-        <h4>Catatan</h4>
-
-        <div class="col-12 col-s-12 commentbox">
-
-            <p>Hey Jackie! How have you been?</p>
-            <p>What a surprise. I haven’t seen you in a long time. How have you been?</p>
-            <p>I’m doing very well. How about you?</p>
-            <p>Well, I finally have some free time. I’m busy juggling between my full time job and
-                freelance projects. But now since it’s done, I can relax for a while.</p>
-        </div>
-
-        <form class="form_komen" action="#">
-            <small>Text</small>
-            <div class="form-group">
-                <input type="text" class="form-control" id="komen" placeholder="Ketik Disini">
-            </div>
-
-            <button class="btn btn-success" onclick="#">Chat</button>
-        </form>
     </div>
 
     <div class="footer">
