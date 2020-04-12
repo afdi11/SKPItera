def5c6c97c8da8c1999b68b73243c079cc64cbf9
Dhiko <dhikojjp@gmail.com>
2020-04-12 14:44:46 +0700

    mhs unduh panel

--- Web_Sistem_KP/resources/views/mahasiswa/mhs_unduh.blade.php
+++ Web_Sistem_KP/resources/views/mahasiswa/mhs_unduh.blade.php
@@ -1,33 +1,46 @@
 @extends('layouts.mahasiswa.mahasiswa_index')
 
 @section('content')
-        <div class="col-9 col-s-9 content">
+<div class="col-9 col-s-9 content">
+    <div class="panel panel-primary">
+        <div class="panel-heading">Unduh File</div>
+        <div class="panel-body">
 
             <div class="col-12 col-s-12 content_satu">
-                <div class="col-2 col-s-12 nilai_akhir">
+                <div class="col-3 col-s-12 nilai_akhir">
                     <p>Penilaian Instansi</p>
                     <a href="{{ asset('/requirement/KP3-Form-Penilaian-Instansi-KP-REV.docx') }}">Unduh</a>
                 </div>
+            </div>
 
-                <div class="col-2 col-s-12 semester">
+            <div class="col-12 col-s-12 content_satu">
+                <div class="col-3 col-s-12 semester">
                     <p>Seminar</p>
                     <a href="{{ asset('/requirement/KP4-Form-Pendaftaran-Seminar-KP-IF-REV.docx') }}">Unduh</a>
                 </div>
+            </div>
 
-                <div class="col-2 col-s-12 catatan">
+            <div class="col-12 col-s-12 content_satu">
+                <div class="col-3 col-s-12 catatan">
                     <p>Presensi</p>
                     <a href="{{ asset('/requirement/KP2-Form-Presensi-dan-Log-Sheet-KP-IF-REV-40.docx') }}">Unduh</a>
                 </div>
+            </div>
 
-                <div class="col-2 col-s-12 semester">
+            <div class="col-12 col-s-12 content_satu">
+                <div class="col-3 col-s-12 semester">
                     <p>Ceklis Kelengkapan</p>
                     <a href="{{ asset('/requirement/C1-Ceklis-Kelengkapan-Seminar-Kerja-Praktik.docx') }}">Unduh</a>
                 </div>
+            </div>
 
-                <div class="col-2 col-s-12 catatan">
+            <div class="col-12 col-s-12 content_satu">
+                <div class="col-3 col-s-12 catatan">
                     <p>Log Sheet</p>
                     <a href="{{ asset('/requirement/KP2-Form-Presensi-dan-Log-Sheet-KP-IF-REV-40.docx') }}">Unduh</a>
                 </div>
             </div>
         </div>
+    </div>
+</div>
 @endsection
