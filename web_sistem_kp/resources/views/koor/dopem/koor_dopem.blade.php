0c711ada343a9d22a74cdca6e5742e108dcb0008
Dhiko <dhikojjp@gmail.com>
2020-04-12 10:44:58 +0700

    dopem search bar

--- web_sistem_kp/resources/views/koor/dopem/koor_dopem.blade.php
+++ web_sistem_kp/resources/views/koor/dopem/koor_dopem.blade.php
@@ -1,22 +1,30 @@
 @extends('layouts.koor.koor_index')
 
 @section('content')
-    <div class="col-9 col-s-12 content">
+<div class="col-9 col-s-12 content">
+    <!-- Actual search box -->
+    <div class="form-group has-feedback has-search">
+        <span class="glyphicon glyphicon-search form-control-feedback"></span>
+        <input type="text" class="form-control" placeholder="Search">
+    </div>
+
+    <div class="table-responsive">
         <table class=" col-12 col-s-12 table table-hover">
             <tr>
-                <th width="70%">Nama Dosen Pembimbing</th>
-                <th width="30%">Lihat</th>
+                <th width="90%">Nama</th>
+                <th width="10%">Lihat</th>
             </tr>
             @foreach ($result as $row)
-                <tr>
-                    <td>{{$row->name}}</td>
-                    <td>
-                        <a href="{{route('koor.dopem.show',$row->id)}}" class="modal-show" id="{{$row->name}}" title="Data Dosen Pembimbing">
-                            <button class="btn btn-info btn-xs">lihat</button>
-                        </a>
-                    </td>
-                </tr>
+            <tr>
+                <td>{{$row->name}}</td>
+                <td>
+                    <a href="{{route('koor.dopem.show',$row->id)}}" class="modal-show" id="{{$row->name}}" title="Data Dosen Pembimbing">
+                        <button class="btn btn-info btn-xs">Lihat</button>
+                    </a>
+                </td>
+            </tr>
             @endforeach
         </table>
     </div>
+</div>
 @endsection
