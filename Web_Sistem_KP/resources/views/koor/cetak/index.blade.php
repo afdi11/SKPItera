75ce9029f6d2efc16bbc870e4cdef1d434cc00aa
Dhiko <dhikojjp@gmail.com>
2020-04-12 10:39:51 +0700

    cetak search bar

--- Web_Sistem_KP/resources/views/koor/cetak/index.blade.php
+++ Web_Sistem_KP/resources/views/koor/cetak/index.blade.php
@@ -3,28 +3,35 @@
 @section('content')
 <div class="col-9 col-s-12 content">
 
-<table class=" col-12 col-s-12 table table-hover">
-    <tr>
-        <th width="30%">Nama</th>
-        <th width="30%">Judul Seminar</th>
-        <th width="20%">Nilai</th>
-        <th width="20%">Lainnya</th>
-    </tr>
-        @foreach($user as $row)
-        <tr>
-            <td>{{$row->name}}</td>
-            <td>{{$row->mahasiswa->seminar['name']}}</td>
-            <td>{{$row->mahasiswa->seminar['nilai']}}</td>
-            <td>
-                <a href="{{route('koor.cetak.show',$row->id)}}" class="view_cetak_lihat" id="{{$row->name}}" title="Data Mahasiswa" >
-                    <button class="btn btn-info btn-xs">lihat</button>
-                </a>
-            </td>
-        </tr>
-        @endforeach
-</table>
+    <!-- Actual search box -->
+    <div class="form-group has-feedback has-search">
+        <span class="glyphicon glyphicon-search form-control-feedback"></span>
+        <input type="text" class="form-control" placeholder="Search">
+    </div>
 
-<button type="submit" class="btn btn-primary" data-dismiss="modal">Cetak</button>
+    <div class="table-responsive">
+        <table class=" col-12 col-s-12 table table-hover">
+            <tr>
+                <th width="30%">Nama</th>
+                <th width="40%">Judul Seminar</th>
+                <th width="20%">Nilai</th>
+                <th width="10%">Lihat</th>
+            </tr>
+            @foreach($user as $row)
+            <tr>
+                <td>{{$row->name}}</td>
+                <td>{{$row->mahasiswa->seminar['name']}}</td>
+                <td>{{$row->mahasiswa->seminar['nilai']}}</td>
+                <td>
+                    <a href="{{route('koor.cetak.show',$row->id)}}" class="view_cetak_lihat" id="{{$row->name}}" title="Data Mahasiswa">
+                        <button class="btn btn-info btn-xs">Lihat</button>
+                    </a>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
+    <button type="submit" class="btn btn-success" data-dismiss="modal">Cetak</button>
 
 </div>
 @endsection
