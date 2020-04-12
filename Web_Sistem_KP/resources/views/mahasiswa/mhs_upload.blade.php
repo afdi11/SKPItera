a5209954b84038b534333e429b5b82e79e224712
Dhiko <dhikojjp@gmail.com>
2020-04-12 14:48:21 +0700

    upload file panel

--- Web_Sistem_KP/resources/views/mahasiswa/mhs_upload.blade.php
+++ Web_Sistem_KP/resources/views/mahasiswa/mhs_upload.blade.php
@@ -1,23 +1,31 @@
 @extends('layouts.mahasiswa.mahasiswa_index')
 
 @section('content')
-        <div class="col-9 col-s-9 content">
-
+<div class="col-9 col-s-9 content">
+    <div class="panel panel-primary">
+        <div class="panel-heading">Upload File</div>
+        <div class="panel-body">
             <div class="col-12 col-s-12 content_satu">
                 <div class="col-3 col-s-12 nilai_akhir">
                     <p>Laporan Kerja Praktik</p>
                     <a href="#">Upload</a>
                 </div>
+            </div>
 
+            <div class="col-12 col-s-12 content_satu">
                 <div class="col-3 col-s-12 semester">
                     <p>Nilai Instansi</p>
                     <a href="#">Upload</a>
                 </div>
+            </div>
 
+            <div class="col-12 col-s-12 content_satu">
                 <div class="col-3 col-s-12 catatan">
                     <p>Log Sheet</p>
                     <a href="#">Upload</a>
                 </div>
             </div>
         </div>
+    </div>
+</div>
 @endsection
