f6e95285bd8e39ba38c280a87a1cd8715faef998
Dhiko <dhikojjp@gmail.com>
2020-04-12 14:25:01 +0700

    perbaikan homepage mahasiswa

--- Web_Sistem_KP/resources/views/mahasiswa/index.blade.php
+++ Web_Sistem_KP/resources/views/mahasiswa/index.blade.php
@@ -2,31 +2,35 @@
 
 @section('content')
 <div class="col-9 col-s-12 content">
+    <div class="panel panel-primary">
+        <div class="panel-heading">Keterangan Mahasiswa</div>
+        <div class="panel-body">
+            <div class="col-9 col-12 content_satu">
+                <div class="col-3 col-s-12 semester">
+                    <p>Semester</p>
+                    <h2>7</h2>
+                </div>
 
-    <div class="col-9 col-12 content_satu">
-        <div class="col-3 col-s-12 semester">
-            <p>Semester</p>
-            <h2>7</h2>
-        </div>
-
-        <div class="col-3 col-s-12 catatan">
-            <p>Status Kerja Praktik</p>
-            <h2>Aktif</h2>
+                <div class="col-3 col-s-12 catatan">
+                    <p>Status Kerja Praktik</p>
+                    <h2>Aktif</h2>
+                </div>
+            </div>
         </div>
     </div>
 
+    <div class="panel panel-primary">
+        <div class="panel-heading">Catatan Dosen</div>
+        <div class="pemisah">
+            <div class="panel-body">
 
-    <div class="col-12 col-s-12 content_dua">
+                <p>Hey Jackie! How have you been?</p>
+                <p>What a surprise. I haven’t seen you in a long time. How have you been?</p>
+                <p>I’m doing very well. How about you?</p>
+                <p>Well, I finally have some free time. I’m busy juggling between my full time job and
+                    freelance projects. But now since it’s done, I can relax for a while.</p>
 
-        <h4>Catatan Dosen</h4>
-
-        <div class="col-12 col-s-12 commentbox">
-
-            <p>Hey Jackie! How have you been?</p>
-            <p>What a surprise. I haven’t seen you in a long time. How have you been?</p>
-            <p>I’m doing very well. How about you?</p>
-            <p>Well, I finally have some free time. I’m busy juggling between my full time job and
-                freelance projects. But now since it’s done, I can relax for a while.</p>
+            </div>
         </div>
     </div>
 </div>
