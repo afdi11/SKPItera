cc6eeda88c3588dcf3bee7d2beb7fd41a82c4e49
Dhiko <dhikojjp@gmail.com>
2020-04-12 10:34:42 +0700

    search bar validasi

--- Web_Sistem_KP/resources/views/koor/validasi/index.blade.php
+++ Web_Sistem_KP/resources/views/koor/validasi/index.blade.php
@@ -1,36 +1,41 @@
 @extends('layouts.koor.koor_index')
 
 @section('content')
-    <div class="col-9 col-s-12 content">
-        <table id="datatables" class=" col-12 col-s-12 table table-hover">
-            <thead>
-                <tr>
-                    <th>Mahasiswa</th>
-                    <th colspan="3">Lainnya</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($result as $row)
-                    <tr>
-                        <td width="80%">{{$row->name}}</td>
-                        <td>
-                            <a href="{{route('koor.validasi.edit',$row->id)}}" class="edit" fungsi="validasi" id="{{$row->id}}" title="Validasi Pendaftaran Mahasiswa">
-                                <button class="btn btn-info btn-xs">Validasi</button>
-                            </a>
-                        </td>
-                        <td>
-                            <a href="{{route('koor.assign.edit',$row->id)}}" class="assign" fungsi="assign" id="{{$row->id}}" title="Assign Dosen Pembimbing">
-                                <button class="btn btn-info btn-xs">Assign</button>
-                            </a>
-                        </td>
-                        <td>
-                            <a href="{{route('koor.mahasiswa.show',$row->id)}}" class="view_data_lihat" id="{{$row->id}}" title="Data Mahasiswa">
-                                <button class="btn btn-info btn-xs">Lihat</button>
-                            </a>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+<div class="col-9 col-s-12 content">
+    <!-- Actual search box -->
+    <div class="form-group has-feedback has-search">
+        <span class="glyphicon glyphicon-search form-control-feedback"></span>
+        <input type="text" class="form-control" placeholder="Search">
     </div>
+    <table id="datatables" class=" col-12 col-s-12 table table-hover">
+        <thead>
+            <tr>
+                <th>Mahasiswa</th>
+                <th colspan="3">Lainnya</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($result as $row)
+            <tr>
+                <td width="80%">{{$row->name}}</td>
+                <td>
+                    <a href="{{route('koor.validasi.edit',$row->id)}}" class="edit" fungsi="validasi" id="{{$row->id}}" title="Validasi Pendaftaran Mahasiswa">
+                        <button class="btn btn-info btn-xs">Validasi</button>
+                    </a>
+                </td>
+                <td>
+                    <a href="{{route('koor.assign.edit',$row->id)}}" class="assign" fungsi="assign" id="{{$row->id}}" title="Assign Dosen Pembimbing">
+                        <button class="btn btn-info btn-xs">Assign</button>
+                    </a>
+                </td>
+                <td>
+                    <a href="{{route('koor.mahasiswa.show',$row->id)}}" class="view_data_lihat" id="{{$row->id}}" title="Data Mahasiswa">
+                        <button class="btn btn-info btn-xs">Lihat</button>
+                    </a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
