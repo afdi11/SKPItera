943cec2222925a454889971057aa443b27d5e347
Dhiko <dhikojjp@gmail.com>
2020-04-12 16:30:55 +0700

    dopem mhs perbaikan

--- web_sistem_kp/resources/views/dosen/Mahasiswa/dopem_mhs.blade.php
+++ web_sistem_kp/resources/views/dosen/Mahasiswa/dopem_mhs.blade.php
@@ -12,12 +12,30 @@
             <div class="table-responsive">
                 <table class=" col-12 col-s-12 table table-hover">
                     <tr class="active">
-                        <th width="90%">Mahasiswa</th>
-                        <th width="10%">Lainnya</th>
+                        <th width="40%">Mahasiswa</th>
+                        <th width="25%">Nilai Seminar</th>
+                        <th width="25%">Nilai Instansi</th>
+                        <th width="10%">Lihat</th>
                     </tr>
                     @foreach($user as $row)
                     <tr>
                         <td>{{$row->name}}</td>
+                        <td>
+                            <div class="form-check">
+                                <label class="form-check-label" for="check1">
+                                    <input type="checkbox" class="form-check-input" id="check1" name="option1" value="something">
+                                </label>
+                                <a href="#">Lihat Laporan</a>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="form-check">
+                                <label class="form-check-label" for="check2">
+                                    <input type="checkbox" class="form-check-input" id="check2" name="option2" value="something">
+                                </label>
+                                <a href="#">Lihat Laporan</a>
+                            </div>
+                        </td>
                         <td><a href="{{route('dosen.mahasiswa.show',$row->id)}}" type="button" name="view" id="{{$row->name}}" class="btn btn-info btn-xs view_data_lihat">Lihat</a></td>
                     </tr>
                     @endforeach
