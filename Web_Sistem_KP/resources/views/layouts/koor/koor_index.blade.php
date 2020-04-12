866e271b49fc9858a4b1853e607df79ae8699fd6
Dhiko <dhikojjp@gmail.com>
2020-04-12 18:03:10 +0700

    koordinator navbar

--- Web_Sistem_KP/resources/views/layouts/koor/koor_index.blade.php
+++ Web_Sistem_KP/resources/views/layouts/koor/koor_index.blade.php
@@ -38,7 +38,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">KERJA PRAKTIK TEKNIK INFORMATIKA</a>
+                    <a class="navbar-brand" href="#">KOORDINATOR</a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="myNavbar">
