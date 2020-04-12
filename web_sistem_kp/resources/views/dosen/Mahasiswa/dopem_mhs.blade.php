ecd9cb612291545953aa9929e4cc7879a845df97
Dhiko <dhikojjp@gmail.com>
2020-04-12 15:19:47 +0700

    dopem mhs panel

--- web_sistem_kp/resources/views/dosen/Mahasiswa/dopem_mhs.blade.php
+++ web_sistem_kp/resources/views/dosen/Mahasiswa/dopem_mhs.blade.php
@@ -1,18 +1,26 @@
 @extends('layouts.dosen.dosen_index')
 @section('content')
-        <div class="col-9 col-s-12 content">
+<div class="col-9 col-s-12 content">
+    <div class="panel panel-primary">
+        <div class="panel-heading">Daftar Mahasiswa</div>
+        <div class="panel-body">
 
-            <table class=" col-12 col-s-12 table table-hover">
-                <tr>
-                    <th width="70%">Mahasiswa</th>
-                    <th width="30%">Lainnya</th>
-                </tr>
+            <div class="table-responsive">
+                <table class=" col-12 col-s-12 table table-hover">
+                    <tr class="active">
+                        <th width="90%">Mahasiswa</th>
+                        <th width="10%">Lainnya</th>
+                    </tr>
                     @foreach($user as $row)
                     <tr>
                         <td>{{$row->name}}</td>
-                        <td><a href="{{route('dosen.mahasiswa.show',$row->id)}}"type="button" name="view" id="{{$row->name}}" class="btn btn-info btn-xs view_data_lihat" >Lihat</a></td>
+                        <td><a href="{{route('dosen.mahasiswa.show',$row->id)}}" type="button" name="view" id="{{$row->name}}" class="btn btn-info btn-xs view_data_lihat">Lihat</a></td>
                     </tr>
                     @endforeach
-            </table>
+                </table>
+            </div>
+
         </div>
+    </div>
+</div>
 @endsection
