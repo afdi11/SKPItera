320d0edffa96d850f5312969a5f597271bb2c219
Dhiko <dhikojjp@gmail.com>
2020-04-12 11:34:34 +0700

    koor panel

--- Web_Sistem_KP/resources/views/koor/index.blade.php
+++ Web_Sistem_KP/resources/views/koor/index.blade.php
@@ -3,40 +3,45 @@
 @section('content')
 <div class="col-9 col-s-12 content">
 
-    <div class="col-12 col-12 content_satu">
-        <div class="col-3 col-s-12 semester">
-            <p>Jumlah Mahasiswa Telah Kerja Praktik</p>
-            <h2>{{$sudah_kp}}</h2>
-        </div>
+    <div class="panel panel-primary">
+        <div class="panel-heading">Keterangan Mahasiswa</div>
+        <div class="panel-body">
+            <div class="col-12 col-12 content_satu">
+                <div class="col-3 col-s-12 semester">
+                    <p>Jumlah Mahasiswa Telah Kerja Praktik</p>
+                    <h2>{{$sudah_kp}}</h2>
+                </div>
 
-        <div class="col-3 col-s-12 catatan">
-            <p>Jumlah Mahasiswa Belum Kerja Praktik</p>
-            <h2>{{$belum_kp}}</h2>
-        </div>
-    </div>
+                <div class="col-3 col-s-12 catatan">
+                    <p>Jumlah Mahasiswa Belum Kerja Praktik</p>
+                    <h2>{{$belum_kp}}</h2>
+                </div>
+            </div>
 
 
-    <div class="col-12 col-s-12 content_dua">
-        <div class="col-3 col-s-12 semester">
-            <p>Jumlah Mahasiswa Telah Seminar</p>
-            <h2>{{$sudah_seminar}}</h2>
-        </div>
+            <div class="col-12 col-s-12 content_dua">
+                <div class="col-3 col-s-12 semester">
+                    <p>Jumlah Mahasiswa Telah Seminar</p>
+                    <h2>{{$sudah_seminar}}</h2>
+                </div>
 
-        <div class="col-3 col-s-12 catatan">
-            <p>Jumlah Mahasiswa Akan Seminar</p>
-            <h2>{{$akan_seminar}}</h2>
-        </div>
-    </div>
+                <div class="col-3 col-s-12 catatan">
+                    <p>Jumlah Mahasiswa Akan Seminar</p>
+                    <h2>{{$akan_seminar}}</h2>
+                </div>
+            </div>
 
-    <div class="col-12 col-s-12 content_tiga">
-        <div class="col-3 col-s-12 semester">
-            <p>Jumlah Mahasiswa Belum Seminar</p>
-            <h2>{{$belum_seminar}}</h2>
-        </div>
+            <div class="col-12 col-s-12 content_tiga">
+                <div class="col-3 col-s-12 semester">
+                    <p>Jumlah Mahasiswa Belum Seminar</p>
+                    <h2>{{$belum_seminar}}</h2>
+                </div>
 
-        <div class="col-3 col-s-12 catatan">
-            <p>Jumlah Mahasiswa Belum Kerja Praktik</p>
-            <h2>200</h2>
+                <div class="col-3 col-s-12 catatan">
+                    <p>Jumlah Mahasiswa Belum Kerja Praktik</p>
+                    <h2>200</h2>
+                </div>
+            </div>
         </div>
     </div>
 </div>
