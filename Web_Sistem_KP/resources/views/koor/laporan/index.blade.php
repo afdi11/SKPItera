9083291bd81dc3590a2214a5aaf68f920ead9f88
Dhiko <dhikojjp@gmail.com>
2020-04-12 11:56:36 +0700

    laporan panel

--- Web_Sistem_KP/resources/views/koor/laporan/index.blade.php
+++ Web_Sistem_KP/resources/views/koor/laporan/index.blade.php
@@ -3,33 +3,40 @@
 @section('content')
 
 <div class="col-9 col-s-12 content" id="content1">
-    <!-- Actual search box -->
-    <div class="form-group has-feedback has-search">
-        <span class="glyphicon glyphicon-search form-control-feedback"></span>
-        <input type="text" class="form-control" placeholder="Search">
-    </div>
+    <div class="panel panel-primary">
+        <div class="panel-heading">Daftar Laporan</div>
+        <div class="panel-body">
+            <!-- Actual search box -->
+            <div class="form-group has-feedback has-search">
+                <span class="glyphicon glyphicon-search form-control-feedback"></span>
+                <input type="text" class="form-control" placeholder="Search">
+            </div>
 
-    <table class=" col-12 col-s-12 table table-hover">
-        <tr>
-            <th width="30%">Nama</th>
-            <th width="30%">Judul Laporan</th>
-            <th width="30%">Jadwal Seminar</th>
-            <th width="10%">Lihat</th>
-        </tr>
-        @foreach($row as $R)
-        <tr>
-            @if($R->mahasiswa->laporans['disetujui'] == 1)
-            <td>{{$R->name}}</td>
-            <td>{{$R->mahasiswa->seminar['name']}}</td>
-            @if($R->mahasiswa->seminar['pelaksanaan'] !=NULL )
-            <td>{{$R->mahasiswa->seminar['pelaksanaan']}}</td>
-            @else
-            <td>Belum ditentukan</td>
-            @endif
-            <td><a href="{{route('koor.laporan.show', $R->id )}}" type="button" class="btn btn-info btn-xs">Lihat<a></td>
-            @endif
-        </tr>
-        @endforeach
-    </table>
+            <div class="table-responsive">
+                <table class=" col-12 col-s-12 table table-hover">
+                    <tr>
+                        <th width="30%">Nama</th>
+                        <th width="30%">Judul Laporan</th>
+                        <th width="30%">Jadwal Seminar</th>
+                        <th width="10%">Lihat</th>
+                    </tr>
+                    @foreach($row as $R)
+                    <tr>
+                        @if($R->mahasiswa->laporans['disetujui'] == 1)
+                        <td>{{$R->name}}</td>
+                        <td>{{$R->mahasiswa->seminar['name']}}</td>
+                        @if($R->mahasiswa->seminar['pelaksanaan'] !=NULL )
+                        <td>{{$R->mahasiswa->seminar['pelaksanaan']}}</td>
+                        @else
+                        <td>Belum ditentukan</td>
+                        @endif
+                        <td><a href="{{route('koor.laporan.show', $R->id )}}" type="button" class="btn btn-info btn-xs">Lihat<a></td>
+                        @endif
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
--- web_sistem_kp/resources/views/koor/laporan/koor_laporan_lihat.blade.php
+++ web_sistem_kp/resources/views/koor/laporan/koor_laporan_lihat.blade.php
@@ -2,9 +2,14 @@
 
 @section('content')
 <div class="col-9 col-s-12 content" id="content1">
-<a href="{{route('koor.laporan.index')}}"type="button" class="btn btn-light">Back</a>
-    <br>
-    <br>
-    <iframe src="{{URL::to('/upload')}}/{{$laporan->name}}.pdf" width='100%' height='500px'></iframe>
+    <div class="panel panel-primary">
+        <div class="panel-heading">Laporan</div>
+        <div class="panel-body">
+            <a href="{{route('koor.laporan.index')}}" type="button" class="btn btn-primary">Back</a>
+            <br>
+            <br>
+            <iframe src="{{URL::to('/upload')}}/{{$laporan->name}}.pdf" width='100%' height='500px'></iframe>
+        </div>
+    </div>
 </div>
 @endsection
