c8cbea2c071f1471be01ec9b87eac1ef31435097
Dhiko <dhikojjp@gmail.com>
2020-04-12 08:39:09 +0700

    search bar koor laporan

--- Web_Sistem_KP/resources/views/koor/laporan/index.blade.php
+++ Web_Sistem_KP/resources/views/koor/laporan/index.blade.php
@@ -3,28 +3,33 @@
 @section('content')
 
 <div class="col-9 col-s-12 content" id="content1">
+    <!-- Actual search box -->
+    <div class="form-group has-feedback has-search">
+        <span class="glyphicon glyphicon-search form-control-feedback"></span>
+        <input type="text" class="form-control" placeholder="Search">
+    </div>
 
-<table class=" col-12 col-s-12 table table-hover">
-    <tr>
-        <th width="30%">Nama</th>
-        <th width="30%">Judul Laporan</th>
-        <th width="30%">Jadwal Seminar</th>
-        <th width="10%">Lihat</th>
-    </tr>
+    <table class=" col-12 col-s-12 table table-hover">
+        <tr>
+            <th width="30%">Nama</th>
+            <th width="30%">Judul Laporan</th>
+            <th width="30%">Jadwal Seminar</th>
+            <th width="10%">Lihat</th>
+        </tr>
         @foreach($row as $R)
         <tr>
             @if($R->mahasiswa->laporans['disetujui'] == 1)
-                <td>{{$R->name}}</td>    
-                <td>{{$R->mahasiswa->seminar['name']}}</td>
-                    @if($R->mahasiswa->seminar['pelaksanaan'] !=NULL )
-                        <td>{{$R->mahasiswa->seminar['pelaksanaan']}}</td>
-                    @else
-                        <td>Belum ditentukan</td>
-                    @endif
-                <td><a href="{{route('koor.laporan.show', $R->id )}}"type="button" class="btn btn-info btn-xs" >Lihat<a></td>
+            <td>{{$R->name}}</td>
+            <td>{{$R->mahasiswa->seminar['name']}}</td>
+            @if($R->mahasiswa->seminar['pelaksanaan'] !=NULL )
+            <td>{{$R->mahasiswa->seminar['pelaksanaan']}}</td>
+            @else
+            <td>Belum ditentukan</td>
+            @endif
+            <td><a href="{{route('koor.laporan.show', $R->id )}}" type="button" class="btn btn-info btn-xs">Lihat<a></td>
             @endif
         </tr>
         @endforeach
-</table>
+    </table>
 </div>
 @endsection
