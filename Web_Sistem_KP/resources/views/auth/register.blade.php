339c5de7d25ac4f55dfbf843c0a00ddaea1701a0
Dhiko <dhikojjp@gmail.com>
2020-04-14 08:53:03 +0700

    perbaikan bahasa

--- Web_Sistem_KP/resources/views/auth/register.blade.php
+++ Web_Sistem_KP/resources/views/auth/register.blade.php
@@ -2,24 +2,24 @@
 
 @section('content')
 <div class="col-3 col-s-12 content">
-    <h3 style="text-align: center">REGISTER</h3>
+    <h3 style="text-align: center">Daftar</h3>
     <form class="col-12 col-s-12 form" method="POST" action="{{ route('register') }}">
         @csrf
         <div class="form-group">
             <label for="name">Nama:</label>
-            <input type="name" class="form-control" id="name" placeholder="Enter Name" name="name" required>
+            <input type="name" class="form-control" id="name" placeholder="Masukkan Nama" name="name" required>
         </div>
         <div class="form-group">
             <label for="email">Email:</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter Email" name="email" required>
+            <input type="email" class="form-control" id="email" placeholder="Masukkan Email" name="email" required>
         </div>
         <div class="form-group">
-            <label for="password">Password:</label>
-            <input type="password" class="form-control" id="password" placeholder="Enter Password" name="password" required>
+            <label for="password">Kata Sandi:</label>
+            <input type="password" class="form-control" id="password" placeholder="Masukkan Kata Sandi" name="password" required>
         </div>
         <div class="form-group">
-            <label for="password-confirm">Re-Type Password</label>
-            <input type="password" class="form-control" id="password-confirm" placeholder="Enter Re-Type Password" name="password_confirmation" required autocomplete="new-password">
+            <label for="password-confirm">Ulangi Kata Sandi</label>
+            <input type="password" class="form-control" id="password-confirm" placeholder="Masukkan Ulang Kata Sandi" name="password_confirmation" required autocomplete="new-password">
         </div>
         <div class="form-group form-check">
             <label class="form-check-label">
