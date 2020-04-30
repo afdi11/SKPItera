b59b4a9436ea904469ec623554b812618865f5c3
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2020-04-30 08:43:20 +0700

    FIX Bug

--- web_sistem_kp/resources/views/koor/validasi/koor_validasi_assign.blade.php
+++ web_sistem_kp/resources/views/koor/validasi/koor_validasi_assign.blade.php
@@ -16,12 +16,12 @@
     <tr>
         <td width="30%"><label>Ubah Dosen Pembimbing</label></td>  
         <td width="70%">
-            <form action= "{{route('koor.assign.update',$user->mahasiswa->id)}}" method="POST">
+            <form action= "{{route('koor.assign.update',$user->mahasiswa['id'])}}" method="POST">
             @csrf    
             {{method_field('PUT')}}
                 <select id="dosen_id" name="dosen_id" placeholder="Assign Dosen Pembimbing">
                     @foreach($dopem as $p)
-                            <option value="{{$p->dosen->id}}" @if($p->id == $pembimbing->id) selected @endif>{{$p->name}}</option>
+                            <option value="{{$p->dosen['id']}}" @if($p->id == $pembimbing->id) selected @endif>{{$p->name}}</option>
                     @endforeach
                 </select>
             </form>
