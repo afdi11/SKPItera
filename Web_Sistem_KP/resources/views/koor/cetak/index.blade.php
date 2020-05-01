01f3e0ab2a6aec874ff7575c6beda16cb604bd0b
Dhiko <dhikojjp@gmail.com>
2020-05-01 18:53:28 +0700

    koor cetak pagination

--- Web_Sistem_KP/resources/views/koor/cetak/index.blade.php
+++ Web_Sistem_KP/resources/views/koor/cetak/index.blade.php
@@ -12,46 +12,50 @@
             </div> -->
 
             <div class="table-responsive">
-                <table class=" col-12 col-s-12 table table-hover">
-                    <tr>
-                        <th width="30%">Nama</th>
-                        <th width="40%">Judul Seminar</th>
-                        <th width="20%">Nilai</th>
-                        <th width="10%">Lihat</th>
-                    </tr>
-                    @foreach($user as $row)
-                    @php ($nilaiAkhir=(0.6*$row->mahasiswa->seminar['nilai'])+(0.4*$row->mahasiswa->instansi()->first()['pivot']['nilai']))
-                    <tr>
-                        <td>{{$row->name}}</td>
-                        <td>{{$row->mahasiswa->seminar['name']}}</td>
-                        <td>
-                            @if($nilaiAkhir>=80)
+                <table table id="datatables" class="table table-striped table-bordered table-sm">
+                    <thead>
+                        <tr class="active">
+                            <th class="th-sm" width="30%">Nama</th>
+                            <th class="th-sm" width="40%">Judul Seminar</th>
+                            <th class="th-sm" width="20%">Nilai</th>
+                            <th class="th-sm" width="10%">Lihat</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($user as $row)
+                        @php ($nilaiAkhir=(0.6*$row->mahasiswa->seminar['nilai'])+(0.4*$row->mahasiswa->instansi()->first()['pivot']['nilai']))
+                        <tr>
+                            <td>{{$row->name}}</td>
+                            <td>{{$row->mahasiswa->seminar['name']}}</td>
+                            <td>
+                                @if($nilaiAkhir>=80)
                                 @php ($huruf="A")
-                            @elseif($nilaiAkhir>=70)
+                                @elseif($nilaiAkhir>=70)
                                 @php ($huruf="AB")
-                            @elseif($nilaiAkhir>=60)
+                                @elseif($nilaiAkhir>=60)
                                 @php ($huruf="B")
-                            @elseif($nilaiAkhir>=50)
+                                @elseif($nilaiAkhir>=50)
                                 @php ($huruf="BC")
-                            @elseif($nilaiAkhir>=40)
+                                @elseif($nilaiAkhir>=40)
                                 @php ($huruf="C")
-                            @elseif($nilaiAkhir>=30)
+                                @elseif($nilaiAkhir>=30)
                                 @php ($huruf="D")
-                            @else
+                                @else
                                 @php ($huruf="E")
-                            @endif
-                            {{$huruf}}
-                        </td>
-                        <td>
-                            <a href="{{route('koor.cetak.show',$row->id)}}" class="view_cetak_lihat" id="{{$row->id}}" title="Data Mahasiswa">
-                                <button class="btn btn-info btn-xs">Lihat</button>
-                            </a>
-                        </td>
-                    </tr>
-                    @endforeach
+                                @endif
+                                {{$huruf}}
+                            </td>
+                            <td>
+                                <a href="{{route('koor.cetak.show',$row->id)}}" class="view_cetak_lihat" id="{{$row->id}}" title="Data Mahasiswa">
+                                    <button class="btn btn-info btn-xs">Lihat</button>
+                                </a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
                 </table>
             </div>
-            <a href="{{route('koor.cetak.show',0)}}" >
+            <a href="{{route('koor.cetak.show',0)}}">
                 <button class="btn btn-primary">Cetak</button>
             </a>
 
