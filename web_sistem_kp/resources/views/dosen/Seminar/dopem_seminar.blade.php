dac8392e68464acd55dce96da3ca0801e019647d
Dhiko <dhikojjp@gmail.com>
2020-05-01 20:16:37 +0700

    seminar modal pagination

--- web_sistem_kp/resources/views/dosen/Seminar/dopem_seminar.blade.php
+++ web_sistem_kp/resources/views/dosen/Seminar/dopem_seminar.blade.php
@@ -12,27 +12,31 @@
             </div> -->
 
             <div class="table-responsive">
-                <table class=" col-12 col-s-12 table table-hover">
-                    <tr>
-                        <th width="35%">Nama</th>
-                        <th width="30%">Jadwal Seminar</th>
-                        <th width="20%">Ruangan</th>
-                        <th width="15%">Lihat</th>
-                    </tr>
-                    @foreach($user as $row)
-                    <tr>
-                        <td>{{$row->name}}</td>
-                        <td>{{$row->mahasiswa->seminar['pelaksanaan']}}</td>
-                        <td>{{$row->mahasiswa->seminar['lokasi']}}</td>
-                        <td>
-                            @if($row->mahasiswa->seminar->disetujui != 1)
+                <table id="datatables" class="table table-striped table-bordered table-sm">
+                    <thead>
+                        <tr class="active">
+                            <th class="th-sm" width="35%">Nama</th>
+                            <th class="th-sm" width="30%">Jadwal Seminar</th>
+                            <th class="th-sm" width="20%">Ruangan</th>
+                            <th class="th-sm" width="15%">Lihat</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($user as $row)
+                        <tr>
+                            <td>{{$row->name}}</td>
+                            <td>{{$row->mahasiswa->seminar['pelaksanaan']}}</td>
+                            <td>{{$row->mahasiswa->seminar['lokasi']}}</td>
+                            <td>
+                                @if($row->mahasiswa->seminar->disetujui != 1)
                                 <a href="{{route('dosen.seminar.edit',$row->id)}}" type="button" name="view" fungsi="submit" id="{{$row->seminar['id']}}" class="btn btn-info btn-xs view_data_lihat">Response</a>
-                            @else
+                                @else
                                 <a href="#" type="button" class="btn btn-success btn-xs">Disetujui</a>
-                            @endif
-                        </td>
-                    </tr>
-                    @endforeach
+                                @endif
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
                 </table>
             </div>
         </div>
